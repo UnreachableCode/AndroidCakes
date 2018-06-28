965fd64f3087013ffd2906439f2a7b1581825c0e
UnreachableCode <cfin@protonmail.com>
2018-06-28 19:59:33 +0100

    Move parsecharset from activity in CakeService

--- app/src/main/java/com/waracle/androidtest/CakeService.java
+++ app/src/main/java/com/waracle/androidtest/CakeService.java
@@ -13,7 +13,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import static android.content.ContentValues.TAG;
-import static com.waracle.androidtest.MainActivity.PlaceholderFragment.parseCharset;
 
 /**
  * Created by cmf on 27/06/2018.
@@ -69,5 +68,24 @@ public class CakeService extends AsyncTask<String, Void, JSONArray> {
         }
         // TODO: do something with the feed
         listener.onResultRecieved(result);
+    }
+
+    /**
+     * Returns the charset specified in the Content-Type of this header,
+     * or the HTTP default (ISO-8859-1) if none can be found.
+     */
+    private static String parseCharset(String contentType) {
+        if (contentType != null) {
+            String[] params = contentType.split(",");
+            for (int i = 1; i < params.length; i++) {
+                String[] pair = params[i].trim().split("=");
+                if (pair.length == 2) {
+                    if (pair[0].equals("charset")) {
+                        return pair[1];
+                    }
+                }
+            }
+        }
+        return "UTF-8";
     }
 }
--- app/src/main/java/com/waracle/androidtest/MainActivity.java
+++ app/src/main/java/com/waracle/androidtest/MainActivity.java
@@ -104,28 +104,6 @@ public class MainActivity extends AppCompatActivity {
             //}
         }
 
-
-
-
-        /**
-         * Returns the charset specified in the Content-Type of this header,
-         * or the HTTP default (ISO-8859-1) if none can be found.
-         */
-        public static String parseCharset(String contentType) {
-            if (contentType != null) {
-                String[] params = contentType.split(",");
-                for (int i = 1; i < params.length; i++) {
-                    String[] pair = params[i].trim().split("=");
-                    if (pair.length == 2) {
-                        if (pair[0].equals("charset")) {
-                            return pair[1];
-                        }
-                    }
-                }
-            }
-            return "UTF-8";
-        }
-
         @Override
         public void onResultRecieved(JSONArray array) {
             mAdapter.setItems(array);
